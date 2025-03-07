50d5026b82343059aa6a6c859c8932efee4d2de3
Guy Stephane <gstephane138@gmail.com>
2025-03-07 15:35:50 +0100

    worked out fully, the responsiveness of the app on mobile.

--- app/mortgage-calculator.jsx
+++ app/mortgage-calculator.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import './media-queries.css'
+import "./media-queries.css";
 
 export default function MortgageCalculator() {
   const [amount, setAmount] = useState("");
@@ -46,14 +46,17 @@ export default function MortgageCalculator() {
     setRate("");
     setSelectedOption("repayment");
     setMonthlyPayment(null);
-  }
+  };
   return (
     <div className="main-container flex justify-center items-center min-h-screen bg-gray-100 p-4">
-      <Card className="xsm:w-full md:w-2/3 h-[400px] flex flex-row items-center justify-center rounded-2xl rounded-tl-2xl rounded-bl-2x p-0 xsm:w-full xsm:h-[400px] main-card">
-        <CardContent className="p-6 bg-white xsm:w-2/3 md:w-1/2 info-card rounded-2xl">
+      <Card className="main-card-container xsm:w-full md:w-2/3 h-[400px] flex flex-row items-center justify-center rounded-2xl rounded-tl-2xl rounded-bl-2x p-0 xsm:w-full xsm:h-[400px] main-card">
+        <CardContent className="main-card-container-content p-6 bg-white xsm:w-2/3 md:w-1/2 info-card rounded-2xl">
           <div className="flex justify-between">
             <h2 className="text-xl font-semibold">Mortgage Calculator</h2>
-            <button className="text-sm text-blue-500 cursor-pointer" onClick={handleClear}>
+            <button
+              className="text-sm text-blue-500 cursor-pointer"
+              onClick={handleClear}
+            >
               Clear All
             </button>
           </div>
@@ -119,7 +122,7 @@ export default function MortgageCalculator() {
               </Button>
             </div>
             <Button
-              className="text-white bg-yellow-700 hover:bg-yellow-800 font-medium py-2 rounded-lg mt-4 w-1/2"
+              className="calculator-btn text-white bg-yellow-700 hover:bg-yellow-800 font-medium py-5 rounded-lg mt-4 w-1/2"
               onClick={handleCalculate}
             >
               Calculate Repayments
@@ -139,16 +142,27 @@ export default function MortgageCalculator() {
           )}
         </CardContent>
       </Card>
-      <Card className="responsive-card xsm:w-full md:w-2/3 h-[400px] flex flex-row items-center justify-center rounded-2xl rounded-bl-2x p-0 xsm:w-full xsm:h-[400px]">
-        <CardContent className="responsive-card-content p-0 !h-full bg-gray-900 rounded-2xl flex flex-col items-center justify-center px-5 xsm:hidden md:flex w-full">
-          <h1 className="text-white text-lg"> Results show here </h1>
-          <p className="text-center mt-2 text-gray-400">
-            Complete the form and click on "complete payments" to see what
-            monthly repayments would be.
+      <Card className="responsive-card w-full h-[400px] flex flex-row items-center justify-center p-0 xsm:w-full xsm:h-[400px]">
+        <CardContent className="responsive-card-content p-0 !h-full bg-gray-900 flex flex-col items-center justify-center px-5 xsm:hidden md:flex w-full">
+          <h1 className="text-white text-lg w-full"> Results show here </h1>
+          <p className="mt-2 text-gray-400 w-full">Your Results</p>
+          <p className="mt-2 w-full text-gray-500">
+            Your results are shown below based on the information you provided.
+            To adjust these results, edit the form and click
           </p>
           {monthlyPayment && (
-            <div className="text-white mt-4">
-              Monthly Payment: £{monthlyPayment}
+            <div className="text-white mt-4 bg-gray-800 h-[200px] w-full rounded-2xl p-4">
+              <p className="text-lg text-gray-500">Monthly Payments</p>
+              <p className="text-3xl font-bold text-yellow-500">
+                £{monthlyPayment}
+              </p>
+              <div className="w-full h-[1px] bg-white mt-4" />
+              <p className="mt-2 text-gray-500">
+                You'll repay them over the term
+              </p>
+              <p className="text-3xl font-bold text-yellow-500">
+                £{monthlyPayment}
+              </p>
             </div>
           )}
         </CardContent>
